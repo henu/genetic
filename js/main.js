73cb8a1a0305fc382c6f96f6d970163e5dc385a7
Henrik Heino <henrik.heino@gmail.com>
2016-02-15 02:32:12 +0200

    Ground added.

--- js/main.js
+++ js/main.js
@@ -5,19 +5,33 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Sun
 var sun = new THREE.DirectionalLight(0xffffff, 1);
 sun.position.set(0, 1, 0);
 scene.add(sun);
 
+// Ambient light
 var ambient_light = new THREE.AmbientLight(0x404040);
 scene.add(ambient_light);
 
+// Test cube
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
 var cube = new THREE.Mesh(geometry, material);
+cube.position.y = 2;
 scene.add(cube);
 
+// Ground
+var ground_geom = new THREE.PlaneGeometry(50, 50);
+var ground_mat = new THREE.MeshLambertMaterial({color: 0xffffff});
+var ground = new THREE.Mesh(ground_geom, ground_mat);
+ground.rotation.x = -Math.PI / 2;
+ground.matrixAutoUpdate = false;
+ground.updateMatrix();
+scene.add(ground);
+
 camera.position.z = 5;
+camera.position.y = 2;
 
 function render()
 {
