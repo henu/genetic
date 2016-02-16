aad2063fe004889ebab54893d5cd28115812a6b3
Henrik Heino <henrik.heino@gmail.com>
2016-02-16 03:24:03 +0200

    Added some lines between test nodes.

--- js/main.js
+++ js/main.js
@@ -19,17 +19,29 @@ Genetic.camera.position.y = 1;
 
 // Some test structures
 node1 = new Genetic.Node(0, 1, 0, Genetic.scene);
-node2 = new Genetic.Node(0, 2, 0, Genetic.scene);
+node2 = new Genetic.Node(0, 2, 0.1, Genetic.scene);
 node3 = new Genetic.Node(0.8, 1.5, 0, Genetic.scene);
+line1 = new Genetic.Line(node1, node2, Genetic.scene);
+line2 = new Genetic.Line(node2, node3, Genetic.scene);
+line3 = new Genetic.Line(node3, node1, Genetic.scene);
 Genetic.nodes = [];
 Genetic.nodes.push(node1);
 Genetic.nodes.push(node2);
 Genetic.nodes.push(node3);
+Genetic.lines = [];
+Genetic.lines.push(line1);
+Genetic.lines.push(line2);
+Genetic.lines.push(line3);
 
 Genetic.clock = new THREE.Clock(true);
 
 Genetic.render = function()
 {
+    for (var line_i = 0; line_i < Genetic.lines.length; ++ line_i) {
+        var line = Genetic.lines[line_i];
+        line.updateMatrix();
+    }
+
     requestAnimationFrame(Genetic.render);
     Genetic.renderer.render(Genetic.scene, Genetic.camera);
 
@@ -41,6 +53,10 @@ Genetic.runNodes = function(deltatime)
     for (var node_i = 0; node_i < Genetic.nodes.length; ++ node_i) {
         var node = Genetic.nodes[node_i];
         node.run(deltatime);
+    }
+    for (var line_i = 0; line_i < Genetic.lines.length; ++ line_i) {
+        var line = Genetic.lines[line_i];
+        line.run();
     }
 }
 
--- js/structure.js
+++ js/structure.js
@@ -2,7 +2,7 @@ var Genetic = Genetic || {};
 
 Genetic.Node = function(x, y, z, scene)
 {
-    this.velocity = new THREE.Vector3(0, 0, 0);
+    this.last_pos = new THREE.Vector3(x, y, z);
 
     this.mesh = new THREE.Mesh(Genetic.Node.geom, Genetic.Node.mat);
     this.mesh.castShadow = true;
@@ -16,7 +16,7 @@ Genetic.Node = function(x, y, z, scene)
 Genetic.Node.radius = 0.1;
 Genetic.Node.geom = new THREE.IcosahedronGeometry(Genetic.Node.radius, 1);
 Genetic.Node.mat = new THREE.MeshLambertMaterial({ color: 0x00e0e0 });
-Genetic.Node.bounciness = 0.25;
+Genetic.Node.bounciness = 0.1;
 
 Genetic.Node.prototype.getPosition = function()
 {
@@ -25,12 +25,100 @@ Genetic.Node.prototype.getPosition = function()
 
 Genetic.Node.prototype.run = function(deltatime)
 {
-    this.velocity.y -= 9.81 * deltatime;
-    this.mesh.position.x += this.velocity.x * deltatime;
-    this.mesh.position.y += this.velocity.y * deltatime;
-    this.mesh.position.z += this.velocity.z * deltatime;
+    // Calculate velocity from last and current positions
+    var velocity_x = (this.mesh.position.x - this.last_pos.x) / deltatime;
+    var velocity_y = (this.mesh.position.y - this.last_pos.y) / deltatime;
+    var velocity_z = (this.mesh.position.z - this.last_pos.z) / deltatime;
+
+    // Add gravity
+    velocity_y -= 9.81 * deltatime;
+
+    // Make sure node will not fall through the ground
     if (this.mesh.position.y < Genetic.Node.radius) {
         this.mesh.position.y = Genetic.Node.radius;
-        this.velocity.y = -this.velocity.y * Genetic.Node.bounciness;
+        velocity_y = -velocity_y * Genetic.Node.bounciness;
+        velocity_x *= Genetic.Node.bounciness;
+        velocity_z *= Genetic.Node.bounciness;
+    }
+
+    // Store last position
+    this.last_pos.x = this.mesh.position.x;
+    this.last_pos.y = this.mesh.position.y;
+    this.last_pos.z = this.mesh.position.z;
+
+    // Apply velocity to position
+    this.mesh.position.x += velocity_x * deltatime;
+    this.mesh.position.y += velocity_y * deltatime;
+    this.mesh.position.z += velocity_z * deltatime;
+}
+
+Genetic.Line = function(node1, node2, scene)
+{
+    this.node1 = node1;
+    this.node2 = node2;
+    this.length = node1.getPosition().distanceTo(node2.getPosition());
+
+    this.mesh = new THREE.Mesh(Genetic.Line.geom, Genetic.Line.mat);
+    this.mesh.castShadow = true;
+    this.mesh.receiveShadow = true;
+    this.mesh.matrixAutoUpdate = false;
+    scene.add(this.mesh);
+}
+
+Genetic.Line.prototype.updateMatrix = function()
+{
+    var p1 = this.node1.getPosition();
+    var p2 = this.node2.getPosition();
+
+    // Move to correct position
+    this.mesh.matrix.makeTranslation(p1.x, p1.y, p1.z);
+
+    // Rotate
+    var diff_x = p2.x - p1.x;
+    var diff_y = p2.y - p1.y;
+    var diff_z = p2.z - p1.z;
+    var diff_xz = Math.sqrt(diff_x*diff_x + diff_z*diff_z);
+    var yaw = Math.atan2(-diff_x, -diff_z);
+    var pitch = Math.PI * -0.5 + Math.atan2(diff_y, diff_xz);
+    Genetic.Line.tmp_mat1.makeRotationY(yaw);
+    this.mesh.matrix.multiply(Genetic.Line.tmp_mat1)
+    Genetic.Line.tmp_mat1.makeRotationX(pitch);
+    this.mesh.matrix.multiply(Genetic.Line.tmp_mat1)
+
+    // Set correct length
+    Genetic.Line.tmp_mat1.makeScale(1, this.length, 1);
+    this.mesh.matrix.multiply(Genetic.Line.tmp_mat1)
+
+    // Make it grow up from origin
+    Genetic.Line.tmp_mat1.makeTranslation(0, 0.5, 0);
+    this.mesh.matrix.multiply(Genetic.Line.tmp_mat1)
+}
+
+Genetic.Line.prototype.run = function()
+{
+    var length_now = this.node1.getPosition().distanceTo(this.node2.getPosition());
+    if (length_now > 0.001) {
+        var diff_x = this.node2.getPosition().x - this.node1.getPosition().x;
+        var diff_y = this.node2.getPosition().y - this.node1.getPosition().y;
+        var diff_z = this.node2.getPosition().z - this.node1.getPosition().z;
+
+        var fix = (length_now - this.length) / length_now / 2;
+        if (Math.abs(fix) > 0.001) {
+            var fix_x = diff_x * fix;
+            var fix_y = diff_y * fix;
+            var fix_z = diff_z * fix;
+            this.node1.getPosition().x += fix_x;
+            this.node1.getPosition().y += fix_y;
+            this.node1.getPosition().z += fix_z;
+            this.node2.getPosition().x -= fix_x;
+            this.node2.getPosition().y -= fix_y;
+            this.node2.getPosition().z -= fix_z;
+        }
     }
 }
+
+Genetic.Line.radius = 0.075;
+Genetic.Line.geom = new THREE.CylinderGeometry(Genetic.Line.radius, Genetic.Line.radius, 1, 6, 1, true);
+Genetic.Line.mat = new THREE.MeshLambertMaterial({ color: 0x00e0e0 });
+
+Genetic.Line.tmp_mat1 = new THREE.Matrix4();
